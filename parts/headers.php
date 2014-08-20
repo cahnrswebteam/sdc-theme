a67d6a4a7cf7ecebb5822b841c677f55ebf3cb64
danialbleile <danial.bleile@wsu.edu>
2014-08-20 12:52:08 -0700

    Construct

--- parts/headers.php
+++ parts/headers.php
@@ -1,12 +1,7 @@
-<header id="tf-primary-header">
-	<div class="tf-site-banner">
+<header id="primary-header">
+	<div class="site-banner">
     	<a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a>
     </div>
-    <nav class="tf-header-nav">
+    <nav class="header-nav">
     </nav>
 </header>
-<div id="tf-site-toolbar">
-    <div id="tf-endowment-logo">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/endownment-logo.png" />
-    </div>
-</div>
